ae20ce10715ba0e6d912e9cd0a7ceb6fee152d0f
Michal Hrbáček <85192186+kelachim@users.noreply.github.com>
2024-06-03 18:42:51 +0200

    adding projects

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,6 +1,7 @@
-import { SiVuedotjs, SiTypescript, SiTailwindcss, SiFramer, SiGit, SiPostgresql, SiSupabase, SiNextdotjs, SiDocker, SiRust } from "react-icons/si";
+import { SiVuedotjs, SiTypescript, SiStrapi, SiTailwindcss, SiFramer, SiGit, SiPostgresql, SiSupabase, SiNextdotjs, SiDocker, SiRust } from "react-icons/si";
 import { useTranslation } from "react-i18next";
 import { Playfair_Display } from "next/font/google";
+import Link from "next/link";
 
 const cormorant = Playfair_Display({
   subsets: ['latin'],
@@ -10,6 +11,7 @@ interface Project {
   title: string;
   description: string;
   image: string;
+  link: string
   technologies: string[];
 }
 
@@ -18,29 +20,30 @@ interface ProjectsProps {
 }
 
 const TechnologyIcon = ({ tech }: { tech: string }) => {
-    const icons: { [key: string]: React.FC<{ className?: string }> | undefined } = {
-        "Next.js": SiNextdotjs,
-        Typescript: SiTypescript,
-        TailwindCSS: SiTailwindcss,
-        Framer: SiFramer,
-        Git: SiGit,
-        Postgresql: SiPostgresql,
-        Supabase: SiSupabase,
-        "Vue.js": SiVuedotjs,
-        Docker: SiDocker,
-        Rust: SiRust,
-    };
+  const icons: { [key: string]: React.FC<{ className?: string }> | undefined } = {
+    "Next.js": SiNextdotjs,
+    Typescript: SiTypescript,
+    TailwindCSS: SiTailwindcss,
+    Framer: SiFramer,
+    Strapi: SiStrapi,
+    Git: SiGit,
+    Postgresql: SiPostgresql,
+    Supabase: SiSupabase,
+    "Vue.js": SiVuedotjs,
+    Docker: SiDocker,
+    Rust: SiRust,
+  };
 
-    const IconComponent = icons[tech];
+  const IconComponent = icons[tech];
 
-    return IconComponent ? <span className=" flex items-center justify-center p-1 px-2 rounded-full">
+  return IconComponent ? <span className=" flex items-center justify-center p-1 px-2 rounded-full">
     <IconComponent className="inline-block w-5 h-5" />
     <h1 className="inline-block px-2 text-lg">{tech}</h1>
   </span> : null;
 };
 
 const Projects: React.FC<ProjectsProps> = ({ projects }) => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
   return (
     <main className="w-screen bg-black flex items-center flex-col text-center xl:text-left">
       <div className="text-[2rem] p-1 xs:p-12 block w-full lg:w-2/3 italic">
@@ -48,21 +51,23 @@ const Projects: React.FC<ProjectsProps> = ({ projects }) => {
       </div>
       <div className="flex justify-center items-center flex-wrap w-full lg:w-10/12">
         {projects.map((project, index) => (
+          <Link target="_blank" href={project.link} key={index}>
             <div
-                key={index}
-                className="w-full xs:w-[30rem] bg-gradient-to-bl from-neutral-900 to-neutral-950 border-neutral-700 border-[1.5px] rounded-xl p-2 xs:p-3 m-3 flex flex-col justify-start items-center"
+              key={index}
+              className="w-full xs:w-[30rem] bg-gradient-to-bl from-neutral-900 to-neutral-950 border-neutral-700 border-[1.5px] rounded-xl p-2 xs:p-3 m-3 flex flex-col justify-start items-center"
             >
-                <img src={project.image} alt="" className="w-full h-[14rem] block rounded-xl object-cover bg-black" />
-                <div className="p-2 text-xl">{project.title}</div>
-                <div className="p-1 text-center text-neutral-400 sm:h-24">
+              <img src={project.image} alt="" className="w-full h-[14rem] block rounded-xl object-cover bg-black" />
+              <div className="p-2 text-xl">{project.title}</div>
+              <div className="p-1 text-center text-neutral-400 sm:h-24">
                 {t(project.description)}
-                </div>
-                <div className="py-2 flex gap-2 flex-wrap items-center justify-center">
-              {project.technologies.map((tech) => (
-                <TechnologyIcon key={tech} tech={tech} />
-              ))}
+              </div>
+              <div className="py-2 flex gap-2 flex-wrap items-center justify-center">
+                {project.technologies.map((tech) => (
+                  <TechnologyIcon key={tech} tech={tech} />
+                ))}
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </main>
